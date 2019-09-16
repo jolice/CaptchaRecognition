2c9fff637f254c08f8d75fcab957500251e1317c
Vladimir Alexeev <25826296+riguron@users.noreply.github.com>
2019-09-16 18:07:16 +0300

    travis

--- Model/src/main/java/me/nextgeneric/captcha/repository/UserProfileRepository.java
+++ Model/src/main/java/me/nextgeneric/captcha/repository/UserProfileRepository.java
@@ -4,6 +4,7 @@ import me.nextgeneric.captcha.user.UserProfile;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.persistence.LockModeType;
@@ -13,9 +14,9 @@ public interface UserProfileRepository extends CrudRepository<UserProfile, Integ
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT u FROM UserProfile u WHERE u.id = :userId")
-    UserProfile retrieveForUpdate(@RequestParam(name = "userId") Integer userId);
+    UserProfile retrieveForUpdate(Integer userId);
 
-    Optional<UserProfile> findOneByApiKey(@RequestParam(name = "apiKey") String apiKey);
+    Optional<UserProfile> findOneByApiKey(String apiKey);
 
-    boolean existsByLogin(@RequestParam(name = "login") String login);
+    boolean existsByLogin(String login);
 }
